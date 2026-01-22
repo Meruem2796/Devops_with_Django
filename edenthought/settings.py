4299f6f9b604813eab9beffb5a1e4add6cc7e8c8
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-01-22 04:12:34 +0100

    Edit environment variables Config

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -10,22 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env(".env")
+
 from pathlib import Path
 
-import environ
-import os
-
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
